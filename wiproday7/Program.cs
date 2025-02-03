e333dc2e3ea4b61c7aba7e9a5849b8b0b81b8507
LAXMIKANTHREDDY92 <klaxmikanthreddy92@gmail.com>
2025-02-03 21:01:30 +0530

    Delegates

---
+++ wiproday7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+// declarinmg delegate
+public delegate void MyDelegate(string message);
+class Program
+{
+    public static void ShowMessage(string message)//declaring method that matches delegates signature
+    {
+        Console.WriteLine(message);
+    }
+    static void Main(string[] args)
+    {
+        // creating an instance for the delegate and point it to the Showmessage
+        MyDelegate del = new MyDelegate(ShowMessage);
+        del("laxmi kanth");
+    }
+
+
+}
